c3cc4ca53b231e10d669b675b5da78f681d458f7
quadrant26 <quadrant26@163.com>
2021-11-12 16:42:06 +0800

    feat: 单元测试 async

--- egg/app/controller/king.js
+++ egg/app/controller/king.js
@@ -10,11 +10,11 @@ class KingController extends Controller {
 
   async getGirls() {
     const { ctx } = this;
-    await new Promise( resolve => {
-      setTimeout(()=>{
-        resolve(ctx.body="<h1>杨幂，正在向你走来</h1>")
-      },5000)
-    })
+    await new Promise(resolve => {
+      setTimeout(() => {
+        resolve(ctx.body = '<h1>杨幂，正在向你走来</h1>');
+      }, 5000);
+    });
   }
 }
 
--- egg/test/app/controller/king.test.js
+++ egg/test/app/controller/king.test.js
@@ -2,7 +2,7 @@
 
 const { app } = require('egg-mock/bootstrap');
 
-describe('king yest', () => {
+describe('test/app/controller/king.test.js', () => {
   it('king index', () => {
     return app.httpRequest().get('/my').expect(200)
       .expect('<h1>接天莲叶无穷碧，映日荷花别样红</h1>');
@@ -10,6 +10,6 @@ describe('king yest', () => {
 
   it('king getGirls', async () => {
     await app.httpRequest().get('getGirls').expect(200)
-        .expect('<h1>杨幂，正在向你走来</h1>');
-  })
+      .expect('<h1>杨幂，正在向你走来</h1>');
+  });
 });
